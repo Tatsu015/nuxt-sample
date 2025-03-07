8396868fb14b81d21b26182e9cf921f63bc7e653
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-03-07 22:32:28 +0900

    add navbar button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        aaaaa
+        <NavBar />
         <slot />
     </div>
 </template>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>
-    about
-  </div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
+  <div>about</div>
 </template>
 
 <script lang="ts" setup>
