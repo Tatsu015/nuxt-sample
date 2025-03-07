052898f4cdfdabd0c1fbcdf1089068d8704407c8
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-03-07 22:43:38 +0900

    add menu

---
+++ components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul class="menu bg-base-200 rounded-box w-56">
+    <li>
+      <NuxtLink to="/">Top</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/about">About</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/usage">使い方</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/policy">プライバシーポリシー</NuxtLink>
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="fixed top-0 right-0">
-    <button class="btn btn-square btn-ghost">
+    <button class="btn btn-square btn-ghost" @click="toggleMenu">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
         className="size-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
       </svg>
-
     </button>
+    <Menu v-if="isMenuOpen" @close="isMenuOpen = false" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+import Menu from './Menu.vue';
 
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
 </script>
 
 <style>
